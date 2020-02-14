9258810b7c1e31c191512bb3766c909432ffd87a
Pushpinder Pal Singh <thisisme@pushpinderpalsingh.com>
2020-02-14 19:47:52 +0530

    Add Try Them All VC

---
+++ WebML1.0/Controller/TryThemAll.swift
@@ -0,0 +1,15 @@
+//
+//  TryThemAll.swift
+//  WebML1.0
+//
+//  Created by Pushpinder Pal Singh on 14/02/20.
+//  Copyright © 2020 Pushpinder Pal Singh. All rights reserved.
+//
+
+import UIKit
+
+class TryThemAll : UIViewController{
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ WebML1.0/Controller/TryThemAllResults.swift
@@ -0,0 +1,15 @@
+//
+//  TryThemAllResults.swift
+//  WebML1.0
+//
+//  Created by Pushpinder Pal Singh on 14/02/20.
+//  Copyright © 2020 Pushpinder Pal Singh. All rights reserved.
+//
+
+import UIKit
+
+class TryThemAllResults : UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
